60cb877277e59a7ca1972db99382074a50392989
guardmansduck <100035465@mvla.net>
2025-10-21 20:05:51 +0000

    I forgot to actually write the code for the 1.21.4 file so here it is

--- Beer/src/com/guardmansduck/beer/compat/_12104/BeerCompat12104.java
+++ Beer/src/com/guardmansduck/beer/compat/_12104/BeerCompat12104.java
@@ -1,22 +1,39 @@
-package com.guardmansduck.beer.compat._1214;
+package com.guardmansduck.beer.compat._12104;
 
 import net.minecraft.util.Identifier;
 import java.util.Map;
 import java.util.HashMap;
 
-public class BeerCompatData1214 {
+public class BeerCompatData12104 {
     public static final Map<String, Identifier> ENTITIES = new HashMap<>();
     public static final Map<String, Identifier> BLOCKS = new HashMap<>();
+    public static final Map<String, String> WORLDGEN_STRUCTURES = new HashMap<>();
+    public static final Map<String, String> BUG_FIX_FLAGS = new HashMap<>();
+    public static final Map<String, String> NBT_KEYS = new HashMap<>();
 
     static {
-        // Entities
-        ENTITIES.put("bogged", new Identifier("minecraft:bogged"));
-        ENTITIES.put("breeze", new Identifier("minecraft:breeze"));
+        // === ENTITIES ===
+        ENTITIES.put("creaking", new Identifier("minecraft:creaking"));
 
-        // Blocks
-        BLOCKS.put("vault", new Identifier("minecraft:ominous_vault"));
-        BLOCKS.put("copper_door", new Identifier("minecraft:copper_door"));
-        BLOCKS.put("oak_door", new Identifier("minecraft:oak_door"));
+        // === BLOCKS ===
+        BLOCKS.put("pale_oak", new Identifier("minecraft:pale_oak"));
+        BLOCKS.put("pale_moss", new Identifier("minecraft:pale_moss"));
+        BLOCKS.put("creaking_heart", new Identifier("minecraft:creaking_heart"));
+        BLOCKS.put("eyeblossoms", new Identifier("minecraft:eyeblossoms"));
+
+        // === WORLDGEN STRUCTURES ===
+        WORLDGEN_STRUCTURES.put("pale_oak_tree", "Generates Pale Oak trees in Pale Forest biomes.");
+        WORLDGEN_STRUCTURES.put("pale_moss_patch", "Generates Pale Moss in swampy biomes.");
+        WORLDGEN_STRUCTURES.put("creaking_heart_plant", "Generates Creaking Heart plants in forest biomes.");
+
+        // === BUG FIXES ===
+        BUG_FIX_FLAGS.put("fix_1", "Fixed plant placement and growth issues with Pale Oak and Moss.");
+        BUG_FIX_FLAGS.put("fix_2", "Resolved Eyeblossoms interaction bug with players.");
+
+        // === NBT KEYS ===
+        NBT_KEYS.put("CreakingHeartState", "Tracks animation/state of Creaking Heart plant.");
+        NBT_KEYS.put("EyeblossomBloom", "Tracks whether Eyeblossoms are blooming.");
+        NBT_KEYS.put("PaleOakAge", "Tracks growth stage of Pale Oak trees.");
     }
 
     public static Identifier getEntity(String name) {
@@ -25,5 +42,17 @@ public class BeerCompatData1214 {
 
     public static Identifier getBlock(String name) {
         return BLOCKS.get(name);
+    }
+
+    public static String getWorldgenStructure(String name) {
+        return WORLDGEN_STRUCTURES.get(name);
+    }
+
+    public static String getBugFix(String name) {
+        return BUG_FIX_FLAGS.get(name);
+    }
+
+    public static String getNBTKey(String name) {
+        return NBT_KEYS.get(name);
     }
 }
