72588bb028417946104de1c93e390571dfadedea
guardmansduck <100035465@mvla.net>
2025-10-21 17:36:26 +0000

    Working on 1.21.5 support

--- Beer/src/com/guardmansduck/beer/BeerCompat.java
+++ Beer/src/com/guardmansduck/beer/BeerCompat.java
@@ -1,16 +1,35 @@
 package com.guardmansduck.beer;
 
+import java.lang.reflect.Method;
+import java.util.Set;
+import java.util.HashSet;
+
 public class BeerCompat {
+
     public static void init() {
-        System.out.println("Beer compatibility layer initialized!");
+        System.out.println("Beer 1.21.5 compat layer initialized!");
+        loadModules("_1215");
     }
 
-    // Example: version-specific hooks
-    public static void doSomethingFor1215() {
-        System.out.println("Running code for 1.21.5");
-    }
+    private static void loadModules(String versionPackageSuffix) {
+        String basePackage = "com.epicd.beer.compat." + versionPackageSuffix;
 
-    public static void doSomethingFor120x() {
-        System.out.println("Running code for 1.20.x");
+        try {
+            // Get all classes in the package using ClassLoader
+            // Note: In plain Java, you can’t list packages directly,
+            // so we must manually register module classes OR use a small registry file.
+            Set<Class<?>> modules = ModuleRegistry.getModules(basePackage);
+
+            for (Class<?> moduleClass : modules) {
+                try {
+                    Method initMethod = moduleClass.getMethod("init");
+                    initMethod.invoke(null); // static init
+                } catch (NoSuchMethodException e) {
+                    System.out.println("No init() method in " + moduleClass.getName());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ Beer/src/com/guardmansduck/beer/compat/1215/rendering/RenderModule1215.java
@@ -0,0 +1,11 @@
+package com.guardmansduck.beer.compat._1215.rendering;
+
+public class RenderModule1215 {
+    public static void init() {
+        System.out.println("Render module for 1.21.5 loaded!");
+    }
+
+    public static void renderFeature() {
+        System.out.println("Rendering custom feature for 1.21.5");
+    }
+}
